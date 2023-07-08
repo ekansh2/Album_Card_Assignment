e6a2279b1f9397d624e766032c99de3b2d47f0e1
ekansh sharma <ekanshsharmaht@gmail.com>
2023-07-08 18:29:42 +0000

    final changes

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import './index.css'
+import React, { useState, useEffect } from "react";
 
-export const Header = () => {
+import "./index.css";
+
+export const Header = (props) => {
+  const [searchvalue, setsearchvalue] = useState("");
+
+  const { result, setresult, apicheckflag, setapicheckflag } = props;
+
+  useEffect(() => {
+    console.log(typeof searchvalue);
+    if (!searchvalue) {
+      setapicheckflag(!apicheckflag);
+    }
+    if (searchvalue != "") {
+      let obj = result.find((temp) => temp[0] === searchvalue);
+      if (obj) setresult([obj]);
+    }
+  }, [searchvalue]);
   return (
-    <div className='header'>
-        <div className='logo'>
-        Logo
-        </div>
-        <div className='searchbar'>
-        searchbar
-        </div>
+    <div className="header">
+      <div className="logo">Logo</div>
+      <div className="searchbar">
+        <input
+          type="text"
+          name="Search"
+          placeholder="Search"
+          value={searchvalue}
+          onChange={(event) => {
+            setsearchvalue(event.target.value);
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
